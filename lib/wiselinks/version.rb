f43bc4ab9bd0cb36fd7d2076b0051f4744107446
Igor Alexandrov <igor.alexandrov@gmail.com>
2014-03-17 13:00:06 +0400

    Bumped minor version

--- lib/wiselinks/version.rb
+++ lib/wiselinks/version.rb
@@ -3,8 +3,8 @@
 module Wiselinks
   module Version
     MAJOR = 1
-    MINOR = 1
-    PATCH = 4
+    MINOR = 2
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
